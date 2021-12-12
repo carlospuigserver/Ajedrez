7dca6eb15835b2e15bd183d075929fefeffb7016
carlospuigserver <cpuiggut@myuax.com>
2021-12-12 23:01:40 +0100

    tarea 1

--- ajedrez.py
+++ ajedrez.py
@@ -43,17 +43,9 @@ while True:
 
 return    
 
-partida_ajedrez('partida1.txt')
 
-def tablero(nombre_fichero,n):
-    'Función que muestra por pantalla el tablero n de una partida de ajedrez'
 
-f=open(nombre_fichero,'r')
-tableros=f.read().split('\n')
 
-for i in tableros[n*9,n*9+8]:
-    print(i)
-return
-tablero('partida1.txt', 2)    
+   
 
     
